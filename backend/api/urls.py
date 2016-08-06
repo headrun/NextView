a8ab4457e34e905c68a38945ad110b590a760417
headrun <annadatayeswanth@gmail.com>
2016-08-06 10:48:40 +0530

    modified dropdown as per api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     url(r'^by_volume/', 'api.views.volume', name="volume"),
     url(r'^user_data/', 'api.views.user_data', name="user_data"),
     url(r'^from_to/', 'api.views.from_to', name="from_to"),
-    url(r'^project/', 'api.views.project', name="project"),
     #url(r'^error_upload/','api.views.error_upload', name="error_upload"),
 ]
