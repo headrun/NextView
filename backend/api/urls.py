a60455404db06ae4219781b31ab98c07f8ce9350
syedasifa <syedasifa95@gmail.com>
2016-08-06 15:26:53 +0530

    ui changes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,10 +6,9 @@ urlpatterns = [
     url(r'^board','api.views.dashboard_insert', name="dashboard_insert"),
     url(r'^error_board','api.views.error_insert', name="error_insert"),
     #url(r'^upload/','api.views.upload', name="upload"),
-    url(r'^upload/','api.views.upload', name="upload"),
+    url(r'^upload/','api.views.error_upload', name="error_upload"),
     url(r'^by_volume/', 'api.views.volume', name="volume"),
     url(r'^user_data/', 'api.views.user_data', name="user_data"),
     url(r'^from_to/', 'api.views.from_to', name="from_to"),
-    url(r'^project/', 'api.views.project', name="project"),
     #url(r'^error_upload/','api.views.error_upload', name="error_upload"),
 ]
