67c07894aad7d15c6197cca3bd77298238bd7fc6
syedasifa <syedasifa95@gmail.com>
2017-01-09 09:30:06 +0530

    some more changes in landing page

--- ui/src/js/landing/landing.component.js
+++ ui/src/js/landing/landing.component.js
@@ -5,15 +5,130 @@
          .component("landing", {
 
            "templateUrl": "/js/landing/landing.html",
-           "controller": ['$http',
+           "controller": ['$http','$scope','$rootScope',
 
-           function ($http) {
+           function ($http,$scope,$rootScope) {
 
              var self = this;
 
+             var project = 'api/project/';
+
              self.hideLoading();
 
-            }],
+             $('#Services').hide();
+
+             $('#Widgets').hide();
+
+             $('#Projects').hide();
+
+             $("#data").click(function() {
+
+                self.showLoading();
+
+             });
+
+            $("#Ser").click(function() {
+
+                $("#About").hide();
+
+                $("#Projects").hide();
+
+                $("#Widgets").hide();
+
+                $("#Services").show();
+            });
+
+            $("#Wid").click(function(){
+
+                $("#Services").hide();
+
+                $("#Widgets").show();
+
+                $("#Projects").hide();
+
+                $("#About").hide();
+            });
+
+            $("#Proj").click(function() {
+
+                $("#Widgets").hide();
+
+                $("#Services").hide();
+
+                $("#About").hide();
+
+                $("#Projects").show();
+
+            });
+
+            $("#Abt").click(function() {
+
+                $("#About").show();
+
+                $("#Services").hide();
+
+                $("#Widgets").hide();
+
+                $("#Projects").hide()
+
+            });
+
+            $("#Ser").on('click', function() {
+
+                $("#Abt").removeClass('active');
+            });
+
+            $("#Wid").on('click', function() {
+
+                $("#Abt").removeClass('active');
+            });
+
+            $("#Proj").on('click', function() {
+
+                $("#Abt").removeClass('active');
+            });
+
+
+
+            $http({method:"GET", url:project}).success(function(result){
+
+                self.pro_cen_nam = result.result.list[1]
+
+                /*if (result.result.role == "customer") {
+
+                    var map_list = result.result.list;
+
+                    self.mapping_list = map_list[1];
+
+                }*/
+
+
+                /*if (result.result.role == "team_lead") {
+
+                    var map_list = result.result.list;
+
+                    self.mapping_list = map_list[1];
+
+                }*/
+
+                /*if (result.result['role'] == "center_manager") {
+
+                    var map_list = result.result.list;
+
+                    self.mapping_list = map_list;
+
+                    }*/
+
+                /*if (result.result['role'] == "nextwealth_manager") {
+
+                      var map_list = result.result.list;
+
+                      self.mapping_list = map_list;
+
+                       }*/
+            });
+
+         }],
 
             "bindings": {
 
